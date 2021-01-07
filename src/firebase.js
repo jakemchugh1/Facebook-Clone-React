abe1f3ddd6fecc699d38609ee9a63481db612842
Jacob W. McHugh <31633544+jakemchugh1@users.noreply.github.com>
2021-01-06 22:25:43 -0800

    Facebook-Clone-React
    Facebook clone created with react using Javascript

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDe0b8mS3Bik05im6YBECnQj822mVqn6Uo",
+    authDomain: "facebook-clone-906ba.firebaseapp.com",
+    projectId: "facebook-clone-906ba",
+    storageBucket: "facebook-clone-906ba.appspot.com",
+    messagingSenderId: "1062438604043",
+    appId: "1:1062438604043:web:3faf6c6e569025800a660e",
+    measurementId: "G-YPRW2LVGZB"
+  };
